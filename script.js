758cc3445006eddc9b1e8c5bbb756cf1ceaea38e
[MLouise] <[mlouiseogdoc@gmail.com]>
2018-05-24 10:23:43 +0200

    html-css-js

--- script.js
+++ script.js
@@ -3,13 +3,27 @@ var lastScrollPos = 0;
 
 $(window).on("scroll", function() {
   var st = $(this).scrollTop();
-  /*if(lastScrollPos > st) {$(".bar").css("top", 0);}
-  else {$(".bar").css("top", -100);}
-  lastScrollPos = st;*/
 
   var docH = $(".main-container").height() + 170;
   console.log(st);
   var winH = $(this).height();
   var totalScroll = (st/(docH-winH))*100;
   $(".bar").val(totalScroll);
+});
+
+$(function(){
+var lastScrollTop = 0;
+var delta = 5;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if(Math.abs(lastScrollTop - st) <= delta)
+      return;
+      if (st > lastScrollTop){
+          $("#menu").css({top:'-138px'})
+          .hover(function(){$("#menu").css({top: '0px'})})
+      } else {
+         $("#menu").css({top:'0px'});
+      }
+   lastScrollTop = st;
+});
 });
